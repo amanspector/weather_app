bd39539c5ed6ac670a4c0b81618dff9f4de4d39c
Marc <spectoraman@gmail.com>
2025-10-20 01:52:50 +0530

    add weather UI with basic structure

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -20,6 +20,7 @@ class _HomeScreenState extends State<HomeScreen> {
   String _humidity ='--';
   String _wind_speed='--';
   String _pressure ='--';
+  Map<String, dynamic>? _weatherData;
   @override
   void initState() {
     super.initState();
@@ -62,6 +63,7 @@ class _HomeScreenState extends State<HomeScreen> {
     List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
     String city = placemarks.isNotEmpty?placemarks[0].locality??"unknown" : "unknown";
     final weatherdata = await services_api().fetchWeather(position.latitude, position.longitude);
+
     print("Weather data: $weatherdata");
     // if(weatherdata!= null){
     //   setState(() {
@@ -108,22 +110,13 @@ class _HomeScreenState extends State<HomeScreen> {
     } else {
       setState(() {
         _city = city;
-        _temperature = "--°C";
         _condition = "Unable to fetch weather";
-        _humidity ='--';
-        _wind_speed='--';
-        _pressure ='--';
-        _isLoading = false;
       });
     }
   } catch (e) {
   setState(() {
   _city = "Error fetching location";
-  _temperature = "--°C";
   _condition = "Error";
-  _humidity ='Error';
-  _wind_speed='Error';
-  _pressure ='Error';
   _isLoading = false;
   });
   print("Error in _getCurrentLocation: $e");
@@ -139,118 +132,351 @@ class _HomeScreenState extends State<HomeScreen> {
           child:_isLoading?
               Center(child: CircularProgressIndicator(
                 color: Colors.blueAccent,
-              ),):Padding(
+              ),)
+
+              
+              :SingleChildScrollView(
               padding: EdgeInsets.symmetric(horizontal: 20,vertical: 30),
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.location_on ,color: Colors.blueAccent,),
-                    SizedBox(width: 8,),
-                    Text(_city,style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 28,
-                      fontWeight: FontWeight.bold,
-                    ),)
-                  ],
-                ),
-                SizedBox(height: 40),
-                Icon(
-                  Icons.wb_sunny_rounded,
-                  color: Colors.orangeAccent,
-                  size: 100,
-                ),
-
-                SizedBox(height: 20,),
-
-                Text(
-                  _temperature,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 64,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-
-                Text(
-                  _condition,
-                  style: const TextStyle(
-                    color: Colors.white70,
-                    fontSize: 22,
-                  ),
-                ),
-
-                SizedBox(height: 10),
-
-                Column(
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Icon(Icons.opacity,color: Colors.blueAccent,),
-                        Text(_humidity,style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 40,
-                          fontWeight: FontWeight.bold,
-                        ),),
-                ],
-                    ),
-                  ],
-                ),
-
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.air,color: Colors.white,),
-                    Text(_wind_speed,style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),),
-
-                  ],
-                ),
-                SizedBox(height: 10),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Icon(Icons.speed,color: Colors.redAccent,),
-                    Text(_pressure,style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.bold,
-                    ),),
-                  ],
-                ),
-
-
-                ElevatedButton.icon(
-                  onPressed: _getCurrentLocation,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blueAccent,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 25, vertical: 15),
-                  ),
-                  icon: const Icon(Icons.refresh, color: Colors.white),
-                  label: const Text(
-                    "Refresh Location",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.w600),
-                  ),
-                ),
-        ],
+                buildCurrentWeather(),
+                const SizedBox(height: 20),
+                buildTodayForecast(),
+                const SizedBox(height: 20),
+                buildWeeklyForecast(),
+                const SizedBox(height: 20),
+                buildAirConditions(),
+              ],
+        //       children: [
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: [
+        //             Icon(Icons.location_on ,color: Colors.blueAccent,),
+        //             SizedBox(width: 8,),
+        //             Text("Hidden hehe",style: TextStyle(
+        //               color: Colors.white,
+        //               fontSize: 28,
+        //               fontWeight: FontWeight.bold,
+        //             ),)
+        //           ],
+        //         ),
+        //         SizedBox(height: 40),
+        //         Icon(
+        //           Icons.wb_sunny_rounded,
+        //           color: Colors.orangeAccent,
+        //           size: 100,
+        //         ),
+        //
+        //         SizedBox(height: 20,),
+        //
+        //         Text(
+        //           _temperature,
+        //           style: TextStyle(
+        //             color: Colors.white,
+        //             fontSize: 64,
+        //             fontWeight: FontWeight.bold,
+        //           ),
+        //         ),
+        //
+        //         Text(
+        //           _condition,
+        //           style: const TextStyle(
+        //             color: Colors.white70,
+        //             fontSize: 22,
+        //           ),
+        //         ),
+        //
+        //         SizedBox(height: 10),
+        //
+        //         Column(
+        //           children: [
+        //             Row(
+        //               mainAxisAlignment: MainAxisAlignment.center,
+        //               crossAxisAlignment: CrossAxisAlignment.center,
+        //               children: [
+        //                 Icon(Icons.opacity,color: Colors.blueAccent,),
+        //                 Text(_humidity,style: TextStyle(
+        //                   color: Colors.white,
+        //                   fontSize: 40,
+        //                   fontWeight: FontWeight.bold,
+        //                 ),),
+        //         ],
+        //             ),
+        //           ],
+        //         ),
+        //
+        //         SizedBox(height: 10),
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: [
+        //             Icon(Icons.air,color: Colors.white,),
+        //             Text(_wind_speed,style: TextStyle(
+        //               color: Colors.white,
+        //               fontSize: 40,
+        //               fontWeight: FontWeight.bold,
+        //             ),),
+        //
+        //           ],
+        //         ),
+        //         SizedBox(height: 10),
+        //         Row(
+        //           mainAxisAlignment: MainAxisAlignment.center,
+        //           children: [
+        //             Icon(Icons.speed,color: Colors.redAccent,),
+        //             Text(_pressure,style: TextStyle(
+        //               color: Colors.white,
+        //               fontSize: 40,
+        //               fontWeight: FontWeight.bold,
+        //             ),),
+        //           ],
+        //         ),
+        //
+        //         SizedBox(height: 20),
+        //
+        //         ElevatedButton.icon(
+        //           onPressed: _getCurrentLocation,
+        //           style: ElevatedButton.styleFrom(
+        //             backgroundColor: Colors.blueAccent,
+        //             shape: RoundedRectangleBorder(
+        //               borderRadius: BorderRadius.circular(30),
+        //             ),
+        //             padding: const EdgeInsets.symmetric(
+        //                 horizontal: 25, vertical: 15),
+        //           ),
+        //           icon: const Icon(Icons.refresh, color: Colors.white),
+        //           label: const Text(
+        //             "Refresh",
+        //             style: TextStyle(
+        //                 color: Colors.white, fontWeight: FontWeight.w600),
+        //           ),
+        //         ),
+        // ],
             ),
           ),
       ),
+      bottomNavigationBar: buildBottomNav(),
     );
   }
+
+Widget buildCurrentWeather() {
+
+  final current = _weatherData!['current'];
+  final temp = current['temp'].toStringAsFixed(1);
+  final condition = current['weather'][0]['description'];
+  final icon = current['weather'][0]['icon'];
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Text(
+          _city,style: TextStyle(
+          color: Colors.white,
+          fontSize: 32,
+          fontWeight: FontWeight.bold,
+        ),
+        ),
+
+        SizedBox(height: 8,),
+        Text(
+          "Chance of rain: 0%", // optional dynamic
+          style: const TextStyle(color: Colors.white70, fontSize: 16),
+        ),
+        Container(
+          // decoration: BoxDecoration(
+          //   shape: BoxShape.circle,
+          //   gradient: RadialGradient(colors: [Colors.yellow ,Colors.orangeAccent],
+          //   radius: 0.8),
+          //
+          // ),
+          padding: EdgeInsets.all(45),
+          child: Icon(
+            Icons.wb_sunny_rounded,
+            size: 100,
+            color: Colors.orangeAccent,
+          ),
+        ),
+        SizedBox(height: 25,),
+        Text(_temperature,style: TextStyle(
+          color: Colors.white,
+          fontSize: 64,
+          fontWeight: FontWeight.bold,
+        ),)
+      ],
+    );
+}
+
+  Widget buildTodayForecast() {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF1E2228),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "TODAY’S FORECAST",
+            style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14,
+                fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 12),
+          SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                forecastItem("9:00 AM", Icons.wb_sunny, "25°"),
+                const SizedBox(width: 16),
+                forecastItem("12:00 PM", Icons.wb_sunny, "28°"),
+                const SizedBox(width: 16),
+                forecastItem("3:00 PM", Icons.wb_sunny, "33°"),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget forecastItem(String time, IconData icon, String temp) {
+    return Column(
+      children: [
+        Text(time, style: const TextStyle(color: Colors.white70)),
+        const SizedBox(height: 8),
+        Icon(icon, color: Colors.yellowAccent, size: 40),
+        const SizedBox(height: 8),
+        Text(temp,
+            style: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+
+  Widget buildWeeklyForecast() {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF1E2228),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            "7-DAY FORECAST",
+            style: TextStyle(
+                color: Colors.white70,
+                fontSize: 14,
+                fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 10),
+          dayForecastItem("Today", Icons.wb_sunny, "36/22"),
+          dayForecastItem("Tue", Icons.wb_sunny, "37/21"),
+          dayForecastItem("Wed", Icons.wb_sunny, "37/21"),
+          dayForecastItem("Thu", Icons.cloud, "37/21"),
+          dayForecastItem("Fri", Icons.cloud, "37/21"),
+          dayForecastItem("Sat", Icons.beach_access, "37/21"),
+          dayForecastItem("Sun", Icons.wb_sunny, "37/21"),
+        ],
+      ),
+    );
+  }
+
+  Widget dayForecastItem(String day, IconData icon, String temp) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 7),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(day, style: const TextStyle(color: Colors.white70, fontSize: 16)),
+          Icon(icon, color: Colors.yellow),
+          Text(temp,
+              style: const TextStyle(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildAirConditions() {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xFF1E2228),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "AIR CONDITIONS",
+                style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w500),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text("See more",
+                    style: TextStyle(color: Colors.blueAccent)),
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              airConditionItem("Real Feel", "30°"),
+              airConditionItem("Wind", "0.2 km/h"),
+            ],
+          ),
+          const SizedBox(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              airConditionItem("Chance of rain", "0%"),
+              airConditionItem("UV Index", "3"),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget airConditionItem(String title, String value) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: const TextStyle(color: Colors.white70, fontSize: 14)),
+        const SizedBox(height: 5,width: 75,),
+        Text(value,
+            style: const TextStyle(
+                color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold)),
+      ],
+    );
+  }
+
+  Widget buildBottomNav() {
+    return BottomNavigationBar(
+      backgroundColor: const Color(0xFF1E2228),
+      selectedItemColor: Colors.blueAccent,
+      unselectedItemColor: Colors.white70,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.list), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.cloud), label: ""),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: ""),
+      ],
+    );
+  }
+
 }
 
